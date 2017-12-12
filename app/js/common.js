004569446aa0bdff87f9db8c5e0f107e62ac1167
Tani4ka <tanya-balaban@yandex.ru>
2017-12-12 21:56:27 +0200

    initial project

--- app/js/common.js
+++ app/js/common.js
@@ -19,25 +19,42 @@ $(function() {
     /* Retina cover plugin*/
     //$('.bg-stretch').retinaCover();
 
+    /* Preloader */
+    var hellopreloader = document.getElementById("hellopreloader_preload");
+    function fadeOutnojquery(el){
+        el.style.opacity = 1;
+        var interhellopreloader = setInterval(function(){
+            el.style.opacity = el.style.opacity - 0.05;
+            if (el.style.opacity <=0.05){
+                clearInterval(interhellopreloader);
+                hellopreloader.style.display = "none";}
+        },16);
+    }
+    window.onload = function(){
+        setTimeout(function(){
+            fadeOutnojquery(hellopreloader);
+        },1000);
+    };
+
 
     /* E-mail Ajax Send */
 
     //Documentation & Example: https://github.com/agragregra/uniMail
-    $("form").submit(function() { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function() {
-            alert("Thank you!");
-            setTimeout(function() {
-                // Done Functions
-                th.trigger("reset");
-            }, 1000);
-        });
-        return false;
-    });
+    //$("form").submit(function() { //Change
+    //    var th = $(this);
+    //    $.ajax({
+    //        type: "POST",
+    //        url: "mail.php", //Change
+    //        data: th.serialize()
+    //    }).done(function() {
+    //        alert("Thank you!");
+    //        setTimeout(function() {
+    //            // Done Functions
+    //            th.trigger("reset");
+    //        }, 1000);
+    //    });
+    //    return false;
+    //});
 
 
     /* E-mail Ajax Send example */
@@ -104,8 +121,76 @@ $(function() {
     });
 
 
+    /* disable hover while scrolling */
+
+    //var body = document.body,
+    //  timer;
+    //window.addEventListener('scroll', function() {
+    //    clearTimeout(timer);
+    //    if(!body.classList.contains('disable-hover')) {
+    //        body.classList.add('disable-hover')
+    //    }
+    //    timer = setTimeout(function(){
+    //        body.classList.remove('disable-hover')
+    //    },500);
+    //}, false);
 
                         /***** Aditional scripts *****/
+
+    /* Menu open close */
+
+    var menuBg = $('.menu-bg');
+    var menu = $('.menu');
+    var opener = $('.open');
+    var open = false;
+
+    $('.open').click(function (e) {
+        if (open == false) {
+            menuBg.css({'height': '100vh', 'transform': 'scaleY(1)'});
+            menu.css({'transform': 'translateX(0) scaleY(1)', 'opacity': '1',
+                'transition': 'transform .6s ease-in, opacity .6s ease-in', 'transition-delay': '.5s'});
+            //menu.css({'opacity': '1'});
+            //menu.addClass("flipInY");
+            opener.addClass("open_active");
+            open = true;
+        } else {
+            (window.onresize = function() {
+                if (window.innerWidth < 768) {
+                    menuBg.css({'transform': 'scaleY(0)', 'height': '0', 'transition-delay': '.3s'});
+                    menu.css({'display': 'block;', 'transform': 'translateX(-100px) scaleY(0.6)', 'opacity': '0',
+                        'transition': 'transform .3s ease-in, opacity .5s' +
+                        ' cubic-bezier(0,.86,.48,.98)'});
+                    //menu.css({'opacity': '0'});
+                    //menu.removeClass("flipInY");
+                    opener.removeClass("open_active");
+                    open = false;
+                } else {
+                    menuBg.css({'transform': 'scaleY(1)'});
+                    menu.css({'display': 'flex;', 'transform': 'translate(0)', 'opacity': '1'});
+                    opener.removeClass("open_active");
+                    //menu.removeClass("flipInY");
+                }
+            })()
+        }
+        e.preventDefault();
+    });
+    $('.logo').click(function (e) {
+        e.preventDefault();
+    });
+
+
+
+
+
+    /* Menu active link */
+
+    $('.menu__link').click(function(){
+        $('.menu__link').removeClass("menu__link_active");
+        $(this).addClass("menu__link_active");
+    });
+
+
+
     /* Equalheight without plug plugin */
 
     //(function ($) {
@@ -265,6 +350,15 @@ $(function() {
     //    ths.html(ths.html().replace(/(\w+\'\w+\s+\w+\!)$/, '<span>$1</span>')); // выделяет 2 последнее слово в span
     //});
 
+    // Add two last words in span
+    //$('.skills .heading_wrap .h2').each(function(){
+    //    var $this = $(this), text=$this.text().trim(), words = text.split(/\s+/);
+    //    var lastWords = words.splice(-2);
+    //    var join = lastWords.join(' ');
+    //    words.push('<span class="h2_orange">' + join + '</span>');
+    //    $this.html(words.join(' '));
+    //});
+
     /* Selectize */
     //$('select').selectize();
 
@@ -389,6 +483,165 @@ $(function() {
     //    $('#callback .formname').val($(this).data('form'));
     //});
 
+
+    /* No touch device :hover */
+    //if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+    //    console.log('this is a touch device');
+    //    document.body.classList.add('touch');
+    //} else {
+    //    console.log('this is not a touch device');
+    //    document.body.classList.add('no-touch');
+    //}
+
+
+
+    /* bezier animations */
+
+    var bez = $.bez([.19,1,.22,1]);
+    var bezcss = ".19,1,.22,1";
+    /* Get css3 transition and transform prefixes */
+    function mg_getProperty(arr0, arr1, arr2){
+        var tmp = document.createElement("div");
+        for(var i=0, len=arr0.length; i<len; i++){
+            tmp.style[arr0[i]] = 800;
+            if(typeof tmp.style[arr0[i]] == 'string'){
+                return {
+                    js: arr0[i],
+                    css: arr1[i],
+                    jsEnd: arr2[i]
+                };
+            }
+        }
+        return null;
+    }
+    function mg_getTransition(){
+        var arr0 = ["transition", "msTransition", "MozTransition", "WebkitTransition", "OTransition", "KhtmlTransition"];
+        var arr1 = ["transition", "-ms-transition", "-moz-transition", "-webkit-transition", "-o-transition", "-khtml-transition"];
+        var arr2 = ["transitionend", "MSTransitionEnd", "transitionend", "webkitTransitionEnd", "oTransitionEnd", "khtmlTransitionEnd"];
+        return mg_getProperty(arr0, arr1, arr2);
+    }
+    function mg_getTransform(){
+        var arr0 = ["transform", "msTransform", "MozTransform", "WebkitTransform", "OTransform", "KhtmlTransform"];
+        var arr1 = ["transform", "-ms-transform", "-moz-transform", "-webkit-transform", "-o-transform", "-khtml-transform"];
+        return mg_getProperty(arr0, arr1, []);
+    }
+    function mg_getPerspective(){
+        var arr0 = ["perspective", "msPerspective", "MozPerspective", "WebkitPerspective", "OPerspective", "KhtmlPerspective"];
+        var arr1 = ["perspective", "-ms-perspective", "-moz-perspective", "-webkit-perspective", "-o-perspective", "-khtml-perspective"];
+        return mg_getProperty(arr0, arr1, []);
+    }
+    var transition = mg_getTransition();
+    var transform = mg_getTransform();
+    var perspective = mg_getPerspective();
+
+
+    var example9 = new Mg({    // example9
+        reference:"example9",
+        click:{
+            activated:[0],
+            cycle:true,
+            interactive:true,
+            multiLess:7, multiPlus:7,
+            scrollWheel:true, dragWheel:true
+        }
+    });
+
+    (window.onresize = function() {
+        var xradius = 470;
+        var yradius = 110;
+
+        if (window.innerWidth < 1200) {
+            xradius = 285;
+            yradius = 100;
+        }
+        if (window.innerWidth < 1024) {
+            xradius = 290;
+            yradius = 100;
+        }
+        if (window.innerWidth < 768) {
+            xradius = 155;
+            yradius = 60;
+        }
+        if (window.innerWidth < 480) {
+            xradius = 90;
+            yradius = 60;
+        }
+
+        example9.click.onEvent = function(){
+            var arr = this.multiActivated;
+            var alpha = Math.PI*2/(arr.length);
+            //var xradius = 300;
+            //var yradius = 100;
+            for (var i=0;i<arr.length;i++){
+                var path = $("#"+this.reference+"-item-"+ arr[i]);
+                if(arr[i]==this.activated){
+                    var depth = 0;
+                } else {
+                    var depth = example9.mapDistanceReverse(this.multiPlus, i, arr.length, 0);
+                }
+                //
+                var theta = alpha*(this.activated-arr[i]-depth/6)+1.6; // -depth/6 will give additional distance based on depth: it gives space for activated
+                var x = 20+xradius+Math.cos(theta)*xradius;
+                var y = yradius+Math.sin(theta)*yradius;
+                var w = h = y/4;
+                var scale = 0.2+y/140;
+                if(arr[i]==this.activated){scale = 1.5; y-=30;}
+                path.clearQueue().stop().css("z-index", Math.round(y/10));
+                if(perspective && transition){
+                    path.css(transition.css, transform.css+" 1.3s cubic-bezier("+bezcss+")");
+                    path.css(transform.css, "translate3d("+x+"px,"+y+"px,0) scale("+scale+")");
+                }else{
+                    path.animate({transformJ:'translate('+x+','+y+') scale('+scale+')'},{queue:true, duration:1300, specialEasing: {transformJ:bez}});
+                }
+            }
+            $("#"+this.reference+"-item-"+this.deactivated).removeClass("active");
+            $("#"+this.reference+"-item-"+this.activated).addClass("active").css("z-index",100);
+        }
+
+        example9.click.scrollClick = function(){
+            var path = $("#"+this.reference+"-click-scrollIn");
+            path.addClass("active");
+        }
+        example9.click.scrollMove = function(){
+            var path = $("#"+this.reference+"-click-scrollIn");
+            if(perspective && transition){
+                path.css(transition.css, transform.css+" 0s");
+                path.css(transform.css, "translate3d("+this.scrollPosition+"px,0,0)");
+            }else{
+                path.clearQueue().stop().animate({left:this.scrollPosition},{queue:true, duration:0, specialEasing: {left:bez}});
+            }
+        }
+        example9.click.scrollRelease = function(){
+            var path = $("#"+this.reference+"-click-scrollIn");
+            path.removeClass("active");
+            if(perspective && transition){
+                path.css(transition.css, transform.css+" 1.2s cubic-bezier("+bezcss+") 0s");
+                path.css(transform.css, "translate3d("+this.scrollPosition+"px,0,0)");
+            }else{
+                path.clearQueue().stop().animate({left:this.scrollPosition},{queue:true, duration:300, specialEasing: {left:bez}});
+            }
+        }
+        example9.click.dragMove = function(){
+            var path = $("#"+this.reference+"-click-dragIn");
+            if(perspective && transition){
+                path.css(transition.css, transform.css+" 0s");
+                path.css(transform.css, "translate3d("+this.dragPosition+"px,0,0)");
+            }else{
+                path.clearQueue().stop().animate({left:this.dragPosition},{queue:true, duration:0, specialEasing: {left:bez}});
+            }
+        }
+        example9.click.dragRelease = function(){
+            var path = $("#"+this.reference+"-click-dragIn");
+            if(perspective && transition){
+                path.css(transition.css, transform.css+" 1.2s cubic-bezier("+bezcss+") 0s");
+                path.css(transform.css, "translate3d("+this.dragPosition+"px,0,0)");
+            }else{
+                path.clearQueue().stop().animate({left:this.dragPosition},{queue:true, duration:300, specialEasing: {left:bez}});
+            }
+        }
+        example9.init();
+
+    })();
 
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,88 @@
 var gulp           = require('gulp'),
-		gutil          = require('gulp-util' ),
-		sass           = require('gulp-sass'),
-		browserSync    = require('browser-sync'),
-		concat         = require('gulp-concat'),
-		uglify         = require('gulp-uglify'),
-		cleanCSS       = require('gulp-clean-css'),
-		rename         = require('gulp-rename'),
-		del            = require('del'),
-		imagemin       = require('gulp-imagemin'),
-		cache          = require('gulp-cache'),
-		autoprefixer   = require('gulp-autoprefixer'),
-		ftp            = require('vinyl-ftp'),
-		notify         = require("gulp-notify"),
-		htmlmin 	   = require('gulp-html-minifier'),
-		rsync          = require('gulp-rsync'),
-		csso 		   = require('gulp-csso'),
-		sourcemaps     = require('gulp-sourcemaps');
+	gutil          = require('gulp-util' ),
+	sass           = require('gulp-sass'),
+	browserSync    = require('browser-sync'),
+	concat         = require('gulp-concat'),
+	uglify         = require('gulp-uglify'),
+	cleanCSS       = require('gulp-clean-css'),
+	rename         = require('gulp-rename'),
+	del            = require('del'),
+	imagemin       = require('gulp-imagemin'),
+	cache          = require('gulp-cache'),
+	autoprefixer   = require('gulp-autoprefixer'),
+	ftp            = require('vinyl-ftp'),
+	notify         = require("gulp-notify"),
+	htmlmin 	     = require('gulp-html-minifier'),
+	rsync          = require('gulp-rsync'),
+	csso 		       = require('gulp-csso'),
+	babel          = require('gulp-babel'),
+	autopolyfiller = require('gulp-autopolyfiller'),
+	merge          = require('event-stream').merge,
+	order          = require("gulp-order"),
+	sourcemaps     = require('gulp-sourcemaps');
 
 
 gulp.task('common-js', function() {
-	return gulp.src([
+	var all = gulp.src([
 		'app/js/common.js',
-		])
-	.pipe(concat('common.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('app/js'));
+	])
+	.pipe(babel({
+		presets: ['es2015']
+	}));
+	var polyfills = all.pipe(autopolyfiller('polyfills.js', {
+		browsers: [ 'Android 2.3',
+			'Android 4',
+			'Chrome 20',
+			'Firefox 24',
+			'ie 8',
+			'ie 9',
+			'iOS 6',
+			'Opera 12',
+			'Safari 6']
+	}));
+	return merge(polyfills, all)
+		.pipe(order([
+			'polyfills.js',
+			'all.js'
+		]))
+		.pipe(concat('common.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'));
 });
 
+//gulp.task('common-js', function() {
+//	return gulp.src([
+//		'app/js/common.js',
+//	])
+//	.pipe(concat('common.min.js'))
+//	.pipe(uglify())
+//	.pipe(gulp.dest('app/js'));
+//});
+
+//gulp.task('map-js', function() {
+//	return gulp.src(['app/js/map.js'])
+//		.pipe(concat('map.min.js'))
+//		.pipe(uglify())
+//		.pipe(gulp.dest('app/js'));
+//});
+
+//gulp.task('js', ['common-js', 'map-js'], function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
-		'app/libs/jquery/jquery.min.js',
-		//'app/libs/modernizr/modernizr.js',
-        'app/libs/plugins-scroll/plugins-scroll.js',
-		//'app/libs/retina-cover/retina-cover.js',
-		'app/libs/picturefill/dist/picturefill.min.js',
+		'app/libs/jquery/jquery-3.2.1.min.js',
+		'app/libs/plugins-scroll/plugins-scroll.js',
+
+		'app/libs/minimit-gallery-master/mg.min.js',
+		'app/libs/minimit-gallery-master/jquery.bez.min.js',
+		'app/libs/minimit-gallery-master/jquery.transform.min.js',
+
 
 		'app/js/common.min.js', // Always in the end
-		])
-	.pipe(concat('scripts.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({stream: true}));
+	])
+		.pipe(concat('scripts.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('app/js'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('browser-sync', function() {
@@ -56,15 +98,29 @@ gulp.task('browser-sync', function() {
 
 gulp.task('sass', function() {
 	return gulp.src('app/scss/style.scss')
-	.pipe(sourcemaps.init())
-	.pipe(sass({'bundleExec': true}).on("error", notify.onError()))    //  // sass({outputStyle: 'expand'})
-	.pipe(csso({ restructure: false, sourceMap: true, debug: true }))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(sourcemaps.write())
-	.pipe(autoprefixer(['last 15 versions']))
-	// .pipe(cleanCSS()) // comment while develop
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.reload({stream: true}));
+		.pipe(sourcemaps.init())
+		.pipe(sass({'bundleExec': true}).on("error", notify.onError())) // sass({outputStyle: 'expand'})
+		.pipe(csso({
+			restructure: false,
+			sourceMap: true,
+			debug: true
+		}))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(sourcemaps.write())
+		//.pipe(autoprefixer(['last 15 versions']))
+		.pipe(autoprefixer([
+			'Android 2.3',
+			'Android >= 4',
+			'Chrome >= 20',
+			'Firefox >= 24',
+			'Explorer >= 8',
+			'iOS >= 6',
+			'Opera >= 12',
+			'Safari >= 6'
+		]))
+		//.pipe(cleanCSS()) // comment while develop
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('watch', ['sass', 'js', 'browser-sync'], function() {
@@ -81,28 +137,32 @@ gulp.task('minify', function() {
 
 gulp.task('imagemin', function() {
 	return gulp.src('app/img/**/*')
-	.pipe(cache(imagemin()))    // tiny png - сжимает лучьше, но оставляет артефакты (на 3% болюше в pagespeed insights)
-	.pipe(gulp.dest('dist/img'));
+		.pipe(cache(imagemin()))    // tiny png - сжимает лучьше, но оставляет артефакты (на 3% болюше в pagespeed insights)
+		.pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('build', ['removedist', 'imagemin', 'sass', 'js'], function() {
+gulp.task('build', ['removedist', 'imagemin', 'sass', 'js', 'minify'], function() {
 
 	var buildFiles = gulp.src([
 		//'app/*.html',
 		'app/.htaccess',
-		]).pipe(gulp.dest('dist'));
+	]).pipe(gulp.dest('dist'));
 
 	var buildCss = gulp.src([
 		'app/css/style.min.css',
-		]).pipe(gulp.dest('dist/css'));
+	]).pipe(gulp.dest('dist/css'));
 
 	var buildJs = gulp.src([
 		'app/js/scripts.min.js',
-		]).pipe(gulp.dest('dist/js'));
+		'app/js/modernizr-custom.js',
+		'app/js/html5shiv.min.js',
+		'app/js/html5shiv-printshiv.min.js',
+		//'app/js/map.min.js',
+	]).pipe(gulp.dest('dist/js'));
 
 	var buildFonts = gulp.src([
 		'app/fonts/**/*',
-		]).pipe(gulp.dest('dist/fonts'));
+	]).pipe(gulp.dest('dist/fonts'));
 
 });
 
@@ -117,24 +177,23 @@ gulp.task('deploy', function() {
 	});
 
 	var globs = [
-	'dist/**',
-	'dist/.htaccess',  // .htaccess - for cache
+		'dist/**',
+		'dist/.htaccess',
 	];
 	return gulp.src(globs, {buffer: false})
-	.pipe(conn.dest('/public_html/S-Mitler'));  // change name of progect
-
+		.pipe(conn.dest('/public_html/MyResume'));  // change name of progect
 });
 
 gulp.task('rsync', function() {
 	return gulp.src('dist/**')
-	.pipe(rsync({
-		root: 'dist/',
-		hostname: 'username@yousite.com',
-		destination: 'yousite/public_html/',
-		archive: true,
-		silent: false,
-		compress: true
-	}));
+		.pipe(rsync({
+			root: 'dist/',
+			hostname: 'username@yousite.com',
+			destination: 'yousite/public_html/',
+			archive: true,
+			silent: false,
+			compress: true
+		}));
 });
 
 gulp.task('removedist', function() { return del.sync('dist'); });
